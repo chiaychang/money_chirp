c8127e354259cf702e35e0a913f766eddd59431f
Andreas <Andreas@andreas.attlocal.net>
2017-05-31 20:34:36 -0500

    add function

---
+++ public/js/pagefunc.js
@@ -0,0 +1,157 @@
+$(document).ready(function() {
+	// signup jQuery capture of the sign up form
+	var signUpForm = $("form.signup");
+	var emailInput = $("input#email-input");
+	var passwordInput = $('input#password-input');
+
+	//mambers seach company capture of the form
+	var searchForm = $("form.search-add");
+	var companySearch = $("input#search-input");
+
+	// login jQuery capture of the login form
+	var loginForm = $("form.login");
+
+
+
+	signUpForm.on("submit", function(event) {
+		event.preventDefault();
+		var userData = {
+			email: emailInput.val().trim(),
+			password: passwordInput.val().trim()
+		};
+
+		if (!userData.email || !userData.password) {
+			return;
+		}
+
+		signUpUser(userData.email, userData.password);
+    	emailInput.val("");
+    	passwordInput.val("");
+	});
+
+	loginForm.on("submit", function(event) {
+        event.preventDefault();
+        console.log("success");
+        var userData = {
+            email: emailInput.val().trim(),
+            password: passwordInput.val().trim()
+        };
+        if (!userData.email || !userData.password) {
+            return;
+        }
+
+        loginUser(userData.email, userData.password);
+        emailInput.val("");
+        passwordInput.val("");
+    });
+
+
+	// searchForm.on("submit", function(event) {
+	// 	event.preventDefault();
+
+	// 	var searchData = companySearch.val().trim().toUpperCase();
+
+	// 	// get or post? this may need to be a "put" route since we created the column by association
+	// 	$.post("/api/" + searchData, function(data) {
+	// 		console.log(data);
+	// 	});	
+	// });
+
+
+	$.get("/api/user_data").then(function(data) {
+		console.log(data);
+		$(".member-name").append(data.email);
+	});
+
+
+	function signUpUser(email, password) {
+		$.post("/api/signup", {
+			email: email,
+			password: password
+		}).then(function(data) {
+			window.location.replace(data);
+		}).catch(function(err) {
+			console.log(err);
+		});
+	}
+
+	function loginUser(email, password) {
+        $.post("/api/login", {
+            email: email,
+            password: password
+        }).then(function(data) {
+            window.location.replace(data);
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+
+    ////////////////////JOYS RUN CHAR FUNCTION////////////////////////////
+
+    // function runChart() {
+    //     var ctx = document.getElementById("myChart");
+    //     var myChart = new Chart(ctx, {
+    //         type: 'bar',
+    //         data: {
+    //             labels: companiesArray,
+    //             datasets: [{
+    //                 label: ["Twitter Trending Score"],
+    //                 backgroundColor: [
+    //                     'rgba(255, 99, 132, 0.2)',
+    //                     'rgba(54, 162, 235, 0.2)',
+    //                     'rgba(255, 206, 86, 0.2)',
+    //                     'rgba(75, 192, 192, 0.2)',
+    //                     'rgba(153, 102, 255, 0.2)',
+    //                     'rgba(255, 159, 64, 0.2)'
+    //                 ],
+    //                 borderColor: [
+    //                     'rgba(255,99,132,1)',
+    //                     'rgba(54, 162, 235, 1)',
+    //                     'rgba(255, 206, 86, 1)',
+    //                     'rgba(75, 192, 192, 1)',
+    //                     'rgba(153, 102, 255, 1)',
+    //                     'rgba(255, 159, 64, 1)'
+    //                 ],
+    //                 borderWidth: 1,
+    //                 data: companiesScore
+    //             }],
+    //             options: {
+    //                 responsive: true,
+    //                 maintainAspectRatio: false
+    //             }
+    //         }
+    //     });
+    // }
+
+    ///////////////////////////////////////////////////////////////////////
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
