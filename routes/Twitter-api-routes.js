cade2a458b00fadf856f066539997f0473af27da
Joy Chang <joychang@Joyteki-MacBook-Pro.local>
2017-06-02 23:18:41 -0500

    testing the keys

--- routes/Twitter-api-routes.js
+++ routes/Twitter-api-routes.js
@@ -5,9 +5,14 @@ var Client = require('node-rest-client').Client;
 var client = new Client();
 
 
-//twitter stuff --- move to another file after testing
-var keys = require("../keys.js");
-var accountInfo = keys.twitterKeys;
+// //twitter stuff --- move to another file after testing
+// var keys = require("../keys.js");
+var accountInfo = {
+    consumer_key: '8rdeCnRswldWEIcJj7AmY9251',
+    consumer_secret: 'qxo7jGTj2MuTn1YahDH2P7jb1LDCcWP1GmU5CcXpUMRk3dJRZ6',
+    access_token_key: '839168077031948291-fxevhFQQcqHfpxB1rTa8DPUOhOxb4Eo',
+    access_token_secret: 'Bq0g8LipoNl9HhSNyBrVT9lNJXjy133kULZXZtqHUpuVE'
+}
 var Twitter = require('twitter');
 var client2 = new Twitter(accountInfo);
 
