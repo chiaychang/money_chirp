f52d14a439965279cf0298090ab6a7f70c11044e
Joy Chang <joychang@Joyteki-MacBook-Pro.local>
2017-06-03 00:14:51 -0500

    still the css route

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ app.set("view engine", "handlebars");
 
 // Serve static content for the app from the "public" directory in the application directory.
 // app.use(express.static(__dirname + "/public"));
-// app.use(express.static(__dirname + "/public/assets/image"));
-// app.use(express.static(process.cwd() + '/public'));
-// app.use(logger("/"));
+app.use(express.static(__dirname + "/public/css"));
+app.use(express.static(__dirname + "/public/images"));
+app.use(express.static(__dirname + "/public/js"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(process.cwd() + "/public"));
 
